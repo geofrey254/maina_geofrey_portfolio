a740e492f5d49319512f2b9921e42b8104d9f146
geofrey <devongeoffreymaina@gmail.com>
2024-07-01 09:48:08 +0300

    Hero Section Done

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -25,120 +25,118 @@ export default function Navbar() {
     setmenuOpen(!menuOpen);
   };
   return (
-    <div className="relative">
-      <nav className="navbar fixed w-full h-24 shadow-xl z-5 bg-special">
-        <div className="flex justify-between items-center h-full w-full">
-          {/* Left side of the navbar */}
-          <div className="left-side px-4 md:px-6 2xl:px-16 flex">
-            <h1 className="name text-main font-bold text-xl">MG</h1>
-          </div>
-          {/* Center of Navbar */}
-          <div className="center-nav hidden sm:flex">
-            <ul className="flex space-x-16 text-black">
-              <li className="font-semibold">
-                <Link href="#" className="cursor-default flex">
-                  <FaFileContract className="mr-2" size={25} />
-                  Services
-                </Link>
-              </li>
-              <li className="font-semibold">
-                <Link href="#" className="cursor-default flex">
-                  <CgWebsite className="mr-2" size={25} />
-                  Works
-                </Link>
-              </li>
-              <li className="font-semibold">
-                <Link href="#" className="cursor-default flex">
-                  <MdContactMail className="mr-2" size={25} />
-                  Resume
-                </Link>
-              </li>
-              <li className="font-semibold">
-                <Link href="#" className="cursor-default flex">
-                  <GiSkills className="mr-2" size={25} />
-                  Skills
-                </Link>
-              </li>
-              <li className="font-semibold">
-                <Link href="#" className="cursor-default flex">
-                  <MdOutlineConnectWithoutContact className="mr-2" size={25} />
-                  Contact
-                </Link>
-              </li>
-            </ul>
-          </div>
-          {/* Right side nav */}
-          <div className="right-side flex">
-            <Link
-              href="#"
-              className="nav-btn flex text-white name font-bold mr-4"
-            >
-              Resume <BiDownload className="text-xl ml-2 p-.5" />
-            </Link>
-            <div
-              onClick={handleNav}
-              className="sm:hidden cursor-pointer color-1 mr-4 "
-            >
-              <div>
-                <CgMenuOreos
-                  size={40}
-                  className={!menuOpen ? "flex text-main ease-in" : "hidden"}
-                />
+    <nav className="navbar fixed w-full h-24 z-5 bg-special z-40">
+      <div className="flex justify-between items-center h-full w-full">
+        {/* Left side of the navbar */}
+        <div className="left-side px-4 md:px-6 2xl:px-16 flex">
+          <h1 className="name text-main font-bold text-xl">MG</h1>
+        </div>
+        {/* Center of Navbar */}
+        <div className="center-nav hidden sm:flex">
+          <ul className="flex space-x-16 text-black">
+            <li className="font-semibold">
+              <Link href="#" className="cursor-default flex">
+                <FaFileContract className="mr-2" size={25} />
+                Services
+              </Link>
+            </li>
+            <li className="font-semibold">
+              <Link href="#" className="cursor-default flex">
+                <CgWebsite className="mr-2" size={25} />
+                Works
+              </Link>
+            </li>
+            <li className="font-semibold">
+              <Link href="#" className="cursor-default flex">
+                <MdContactMail className="mr-2" size={25} />
+                Resume
+              </Link>
+            </li>
+            <li className="font-semibold">
+              <Link href="#" className="cursor-default flex">
+                <GiSkills className="mr-2" size={25} />
+                Skills
+              </Link>
+            </li>
+            <li className="font-semibold">
+              <Link href="#" className="cursor-default flex">
+                <MdOutlineConnectWithoutContact className="mr-2" size={25} />
+                Contact
+              </Link>
+            </li>
+          </ul>
+        </div>
+        {/* Right side nav */}
+        <div className="right-side flex">
+          <Link
+            href="#"
+            className="nav-btn flex text-white name font-bold mr-4"
+          >
+            Resume <BiDownload className="text-xl ml-2 p-.5" />
+          </Link>
+          <div
+            onClick={handleNav}
+            className="sm:hidden cursor-pointer color-1 mr-4 "
+          >
+            <div>
+              <CgMenuOreos
+                size={40}
+                className={!menuOpen ? "flex text-main ease-in" : "hidden"}
+              />
 
-                <BiWindowClose
-                  size={40}
-                  className={
-                    menuOpen
-                      ? "flex text-main transition ease-out delay-100"
-                      : "hidden"
-                  }
-                />
-              </div>
+              <BiWindowClose
+                size={40}
+                className={
+                  menuOpen
+                    ? "flex text-main transition ease-out delay-100"
+                    : "hidden"
+                }
+              />
             </div>
           </div>
-
-          {/* Mobile Menu */}
-          <div
-            className={
-              menuOpen
-                ? "fixed nav-top-1 w-[100%] h-96 transition duration-700 ease-in-out bg-nav"
-                : "fixed collapse h-1/2 transition duration-700 ease-in-out"
-            }
-          >
-            <ul className="flex flex-col text-xl space-y-12 pt-6 pl-6">
-              <li className="font-semibold">
-                <Link href="#" className="cursor-default flex">
-                  <FaFileContract className="mr-2" size={25} />
-                  Services
-                </Link>
-              </li>
-              <li className="font-semibold">
-                <Link href="#" className="cursor-default flex">
-                  <CgWebsite className="mr-2" size={25} />
-                  Works
-                </Link>
-              </li>
-              <li className="font-semibold">
-                <Link href="#" className="cursor-default flex">
-                  <MdContactMail className="mr-2" size={25} /> Resume
-                </Link>
-              </li>
-              <li className="font-semibold">
-                <Link href="#" className="cursor-default flex">
-                  <GiSkills className="mr-2" size={25} />
-                  Skills
-                </Link>
-              </li>
-              <li className="font-semibold">
-                <Link href="#" className="cursor-default flex">
-                  <MdOutlineConnectWithoutContact className="mr-2" size={25} />
-                  Contact
-                </Link>
-              </li>
-            </ul>
-          </div>
         </div>
-      </nav>
-    </div>
+
+        {/* Mobile Menu */}
+        <div
+          className={
+            menuOpen
+              ? "fixed nav-top-1 w-[100%] h-96 transition duration-700 ease-in-out bg-nav"
+              : "fixed collapse h-1/2 transition duration-700 ease-in-out"
+          }
+        >
+          <ul className="flex flex-col text-xl space-y-12 pt-6 pl-6">
+            <li className="font-semibold">
+              <Link href="#" className="cursor-default flex">
+                <FaFileContract className="mr-2" size={25} />
+                Services
+              </Link>
+            </li>
+            <li className="font-semibold">
+              <Link href="#" className="cursor-default flex">
+                <CgWebsite className="mr-2" size={25} />
+                Works
+              </Link>
+            </li>
+            <li className="font-semibold">
+              <Link href="#" className="cursor-default flex">
+                <MdContactMail className="mr-2" size={25} /> Resume
+              </Link>
+            </li>
+            <li className="font-semibold">
+              <Link href="#" className="cursor-default flex">
+                <GiSkills className="mr-2" size={25} />
+                Skills
+              </Link>
+            </li>
+            <li className="font-semibold">
+              <Link href="#" className="cursor-default flex">
+                <MdOutlineConnectWithoutContact className="mr-2" size={25} />
+                Contact
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
   );
 }
--- components/index.ts
+++ components/index.ts
@@ -1,6 +1,8 @@
 import Navbar from "./Navbar"
 import Footer from "./Footer"
+import Hero from "./Hero"
 export {
     Navbar,
-    Footer
+    Footer,
+    Hero
 }
